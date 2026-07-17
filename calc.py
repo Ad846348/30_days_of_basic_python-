70456477d05a8781e42b5f61e6048c8fa52c7f41
Ad846348 <aditry471@gmail.com>
2026-07-17 09:51:06 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -10,30 +10,30 @@ def calc():
 7.Exponential
 8.Table of a and b
 9.Exit""")      
-a=int(input("enter the number "))
-b=int(input("enter the number "))
-i=int(input("enter choice "))
-if(i==1):
-      print("sum=",a+b)
-elif(i==2):
-      print("difference=",a-b)
-elif(i==3):
-      print("product=",a*b)
-elif(i==4):
-      print("quotient=",a//b)
-elif(i==5):
-      print("quotient=",a/b)
-elif(i==6):
-      print("remainder",a%b)
-elif(i==7):
-      print("exponential=",a**b)
-elif(i==8):
-          for j in range(1,11):
-              print("table of a=",a*j)
-              print("table of b=",b*j)
-elif (i==9):
-    print("Exit")
-else:
+        a=int(input("enter the number "))
+        b=int(input("enter the number "))
+        i=int(input("enter choice "))
+        if(i==1):
+            print("sum=",a+b)
+        elif(i==2):
+            print("difference=",a-b)
+        elif(i==3):
+            print("product=",a*b)
+        elif(i==4):
+            print("quotient=",a//b)
+        elif(i==5):
+            print("quotient=",a/b)
+        elif(i==6):
+            print("remainder",a%b)
+        elif(i==7):
+            print("exponential=",a**b)
+        elif(i==8):
+        for j in range(1,11):
+            print("table of a=",a*j)
+            print("table of b=",b*j)
+        elif (i==9):
+            print("Exit")
+        else:
           print("invalid")
 
           
