b71fa3b659e94a2939dc11127682592f0e153dde
Ad846348 <aditry471@gmail.com>
2026-07-27 15:21:36 +0530

    Update Atm.GUI.py

--- Mini_Projects /Atm.GUI.py
+++ Mini_Projects /Atm.GUI.py
@@ -1,41 +1,52 @@
-import tkinter as tk
+import tkinter as tk  # spelling thik
 from tkinter import messagebox
-balance=10000
+
+balance = 10000
+
 def show_balance():
-	global balance
-	messagebox.showinfo("Balance",f"Current Balance=Rs{balance}")
-	
+    global balance
+    messagebox.showinfo("Balance", f"Current Balance=Rs{balance}")
+
 def debit_balance():
-	global balance
-	a=int(amount_textbox.get())
-		try:
-			if(a>balance):
-			 messagebox.showinfo("Insufficient balance")
-			 elif(a<=0):
-			 	messagebox.showerror("please enter a valid value")
-			 	else:
-			 		balance=balance-a
-			 		messagebox.showinfo("success",  "successfullydebited")
-		except ValueError:
-			messagebox.showerror("Error","Enter valid value")
-	 	
+    global balance
+    try:  # try sabse upar
+        a = int(amount_textbox.get())
+        if a > balance:
+            messagebox.showinfo("Error", "Insufficient balance")
+        elif a <= 0:
+            messagebox.showerror("Error", "please enter a valid value")
+        else:
+            balance = balance - a
+            messagebox.showinfo("success", "successfully debited")
+    except ValueError:
+        messagebox.showerror("Error", "Enter valid number")
+
 def credit_balance():
-	global balance
-	a=int(amount_textbox.get())
-	balance=balance+a
-	messagebox.showinfo("success", "successfully credited")
-	
-root=tk.Tk()
+    global balance
+    try:
+        a = int(amount_textbox.get())
+        balance = balance + a
+        messagebox.showinfo("success", "successfully credited")
+    except ValueError:
+        messagebox.showerror("Error", "Enter valid number")
+
+root = tk.Tk()
 root.geometry("300x400")
 root.configure(bg="#00FF00")
-amount_label= tk.Label(root,text="Enter amount")
+
+amount_label = tk.Label(root, text="Enter amount")
 amount_label.pack(pady=20)
-amount_textbox= tk.Entry(root)
+
+amount_textbox = tk.Entry(root)
 amount_textbox.pack(pady=20)
-credit_button= tk.Button(root,text="credit",command=credit_balance)
+
+credit_button = tk.Button(root, text="credit", command=credit_balance)
 credit_button.pack(pady=5)
-debit_button= tk.Button(root,text="debit",command=debit_balance)
+
+debit_button = tk.Button(root, text="debit", command=debit_balance)
 debit_button.pack(pady=5)
-show_button= tk.Button(root,text="show",command=show_balance)
+
+show_button = tk.Button(root, text="show", command=show_balance)
 show_button.pack(pady=5)
-root.mainloop()
+
+root.mainloop()  
