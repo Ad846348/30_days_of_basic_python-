1ecda16b7a7109e5797af135caf9d5241a1e0574
Ad846348 <aditry471@gmail.com>
2026-08-01 12:09:55 +0530

    Create Display_box.py

---
+++ Mini_Projects /Display_box.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import scrolledtext
+root=tk.Tk()
+app= tk.Label(root, text="Scrolled text",font=('Georgia',44))
+app.pack(fill=tk.BOTH,expand=True)
+text_scroll_1=scrolledtext.ScrolledText(root,wrap=tk.WORD,height=30,state=tk.DISABLED)
+text_scroll_1.pack()
+text_scroll=scrolledtext.ScrolledText(root,wrap=tk.WORD,height=30)
+text_scroll.pack()
+root.mainloop()
+
