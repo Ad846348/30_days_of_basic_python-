c653cd80848827e808af878e1369db8967277c8d
Ad846348 <aditry471@gmail.com>
2026-08-01 12:14:19 +0530

    Update Display_box.py

--- Mini_Projects /Display_box.py
+++ Mini_Projects /Display_box.py
@@ -3,7 +3,7 @@ from tkinter import scrolledtext
 root=tk.Tk()
 app= tk.Label(root, text="Scrolled text",font=('Georgia',44))
 app.pack(fill=tk.BOTH,expand=True)
-text_scroll_1=scrolledtext.ScrolledText(root,wrap=tk.WORD,height=30,state=tk.DISABLED)
+text_scroll_1=scrolledtext.ScrolledText(root,wrap=tk.WORD,height=30,bg="#B8255F"state=tk.DISABLED)
 text_scroll_1.pack()
 text_scroll=scrolledtext.ScrolledText(root,wrap=tk.WORD,height=30)
 text_scroll.pack()
