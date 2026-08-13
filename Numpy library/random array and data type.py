0acd4fcf6dbd3c6d5d7aed3c6a4ae99e7d25cc3d
Ad846348 <aditry471@gmail.com>
2026-08-13 11:34:04 +0530

    Add files via upload

---
+++ Numpy library/random array and data type.py
@@ -0,0 +1,33 @@
+# Creating random valued array
+
+# Rand()
+
+import numpy as np
+a=np.random.rand(8)
+print(a)
+print()
+
+#Randn()
+
+import numpy as np
+a=np.random.randn(8)
+print(a)
+print()
+
+#Ranf()
+
+import numpy as np
+a=np.random.ranf(8)
+print(a)
+print()
+
+#To find data type
+
+import numpy as np
+a=np.array([1,2,3])
+print("Data type :",a.dtype)
+print()
+
+
+
+
