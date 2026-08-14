859a4985686c1abc8afa8567642ca50f4188c088
Ad846348 <aditry471@gmail.com>
2026-08-14 14:31:43 +0530

    Create Arithmetic operations.py

---
+++ Numpy library/Arithmetic operations.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([10, 20, 30, 40])
+b = np.array([2, 4, 5, 10])
+
+print("a:", a)
+print("b:", b)
+print("-" * 15)
+
+print("Addition a+b:", a + b)     
+print("Subtraction a-b:", a - b)  
+print("Multiply a*b:", a * b)     
+print("Divide a/b:", a / b)       
+print("Power a**2:", a ** 2)      
+
+print("\nEk number se sab me:")
+print("a + 5:", a + 5)            
+print("a * 2:", a * 2)        
