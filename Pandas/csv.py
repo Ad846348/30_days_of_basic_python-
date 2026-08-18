d6d8e1d4fb196b48017d837802745f35d7751c51
Ad846348 <aditry471@gmail.com>
2026-08-18 10:56:29 +0530

    Update csv.py

--- Pandas/csv.py
+++ Pandas/csv.py
@@ -2,3 +2,5 @@ import pandas as pd
 var=pd.DataFrame({"Products":["Laptop","Mouse","Keyboard"], "Price":[50000,5000,3000], "sold":[10,20,5]})
 print(var)
 var.to_csv("Text_new.csv",index=False)
+a=pd.read_csv("Text_new.csv")
+print(a.to_string(index=False))
